43a85aefbdb82ae6c43e73089f627d4155e0f163
TheKitBoi <84044666+TheKitBoi@users.noreply.github.com>
2022-11-30 18:24:34 +0000

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -14,7 +14,7 @@ class Main extends Sprite
 	{
 		super();
 
-		addChild(new FlxGame(1280, 720, PlayState, 1, framerate, framerate, true, false));
+		addChild(new FlxGame(1280, 720, PlayState, #if (flixel < "5.0.0") 1, #end framerate, framerate, true, false));
 		
 		function buildDebugDisplay()
    		{
