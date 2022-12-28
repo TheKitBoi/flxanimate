42a4e906b30608c060ab479b02d9f0ac15e4c682
TheKitBoi <84044666+TheKitBoi@users.noreply.github.com>
2022-12-28 21:43:47 +0000

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -47,6 +47,7 @@ class FPS extends TextField
 		selectable = false;
 		mouseEnabled = false;
 		defaultTextFormat = new TextFormat("_sans", 14, color);
+		displayFormat = '%v FPS';
 		autoSize = LEFT;
 		multiline = true;
 		text = "FPS: ";
